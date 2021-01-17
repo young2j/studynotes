5a4041b790179b3aa2761be198ae52e1cabc1fc5
yangshuangjie <yangshuangjie@magicare.me>
2021-01-18 04:15:37 +0800

    modify gitlabcicd

--- gitlab-cicd/deploy.sh
+++ gitlab-cicd/deploy.sh
@@ -1,6 +1,13 @@
-cd /dockers
+#!/bin/bash
+
 docker-compose down
 docker-compose pull
 docker-compose up -d dev
 noneimgs=$(docker images -aq --filter dangling=true)
-$noneimgs && docker rmi $noneimgs
+len=$(echo -n "$noneimgs" | wc -c)
+if (( $len>0 ))
+then
+  docker rmi $noneimgs
+else
+  echo "there is no images tagged none."
+fi
