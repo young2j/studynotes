3f8c9cf91f3fbce176178037868f29cb82cc2d1a
ysj <ysj@yangsj2-knownsec.local>
2022-09-19 00:29:26 +0800

    feat: 开启<<rust程序设计>>学习

--- Rust/rusta/src/main.rs
+++ Rust/rusta/src/main.rs
@@ -1,12 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess:");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is {secret_number}");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please input your guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {guess}");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("too big!"),
+            Ordering::Less => println!("too small"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
 }
